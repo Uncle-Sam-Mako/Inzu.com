407fb8672f66d8c9a6e7d01aed4b6a961feeeb14
Uncle Sam <18bn037@esisalama.org>
2020-07-29 21:38:19 +0200

    Ajout de la liste de maison dans search.html

--- Js/index.js
+++ Js/index.js
@@ -1,12 +1,5 @@
 //
-const house = document.querySelector('.house-item');
-const townBox = document.querySelector('.town-item.kin');
-for(let i = 0, c = 1; i<=c; i++){
-    const newHouse = house.cloneNode(true);
-    const newItem = townBox.cloneNode(true);
-    townBox.parentNode.appendChild(newItem);
-    house.parentNode.appendChild(newHouse);
-}
+
 //Autocomplete
 $( function(){
     let availableCountries = [
