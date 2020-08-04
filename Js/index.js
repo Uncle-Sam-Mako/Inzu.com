8e95313be899f39b97e028caa4d0a446bdd6c4ec
Uncle Sam <18bn037@esisalama.org>
2020-08-04 09:42:37 +0200

    Adding of more-filters and sub-input

--- Js/index.js
+++ Js/index.js
@@ -16,9 +16,31 @@ $( function(){
 //Loader
 window.addEventListener("load",()=>document.querySelector('#loader-box').className += " hide");
 
-//lazyloading
 $(document).ready(function(){
+    //lazyloading
     $("img.lazy").lazyload({
         effect: "fadeIn"
     });
-})
+
+    let moreFilters = $('.filters .more-filters'),
+    moreFilersBtn = $('#more-filters-btn')
+    moreFilters.hide();
+    moreFilersBtn.click(function(){
+        moreFilters.slideToggle('fast');
+    });
+    //ancre-fluide
+    $('a[href^="#"]').on('click', function(e){
+        e.preventDefault();
+        let link = $(this).attr('href'),
+        elem = $(link);
+        if(elem.length){
+            let elemTop = elem.first().offset().top;
+            $('html, body').animate({
+                scrollTop: elemTop
+            }, "fast");
+        }
+
+    });
+    
+
+});
